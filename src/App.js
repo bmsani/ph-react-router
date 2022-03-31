4b86fab7b0e3dce1ab8d4e216df98389618d7df9
bmsani <bmsani6@gmail.com>
2022-03-31 11:43:02 +0600

    not a route problem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Friends from './Components/Friends/Friends';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-9xl'>Hi it's bm</h1>
+      <h1 className='text-9xl'>Welcome to my Fancy routing website</h1>
+      <Routes>
+        <Route> path="/" element={<Home/>}</Route>
+        <Route> path="/friends" element={<Friends/>}</Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h2>Roni from friends.
+            </h2>
+        </div>
+    );
+};
+
+export default Friends;
